cff4a8395b2317ad6e1679b961899b15fa6ee1b3
Ajeng dpr <ajengpramesti37.ap@gmail.com>
2022-10-11 21:04:19 +0800

    First commit

--- PRAK002-2210817220001-AjengDiahPramesti.c.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("Selamat Pagi, Ajeng Diah Pramesti \n");
-    printf("Selamat Siang, Ajeng Diah Pramesti \n");
-    printf("Selamat Malam, Ajeng Diah Pramesti \n");
-    return 0;
-}
--- PRAK002-2210817220001-AjengDiahPramesti.py.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi, Ajeng Diah Pramesti")
-print("Selamat Siang, Ajeng Diah Pramesti")
-print("Selamat Malam, Ajeng Diah Pramesti")
