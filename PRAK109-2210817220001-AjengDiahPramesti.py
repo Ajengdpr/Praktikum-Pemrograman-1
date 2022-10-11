579a4c0b8cb094dee9c1b15436adac1b3ffd3de4
Ajengdpr <114990194+Ajengdpr@users.noreply.github.com>
2022-10-11 20:47:16 +0800

    Delete PRAK109-2210817220001-AjengDiahPramesti.py

--- PRAK109-2210817220001-AjengDiahPramesti.py
+++
@@ -1,6 +0,0 @@
-Pasukan_Yu_Zhong =  958730
-Jumlah_Pahlawan = 5
-Total_Melawan = Pasukan_Yu_Zhong / Jumlah_Pahlawan
-print("Jumlah pasukan yang dibawa Yu Zhong =", Pasukan_Yu_Zhong)
-print("Jumlah pahlawan =", Jumlah_Pahlawan)
-print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah",round(Total_Melawan), "pasukan")
